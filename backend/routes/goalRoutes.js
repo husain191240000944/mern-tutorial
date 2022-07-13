7f5f3460b87ba892bbe7d777ba11932346211f72
husain191240000944 <191240000944@unisnu.ac.id>
2022-07-13 13:57:40 +0700

    add goalRoutes.js

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
-module.exports = router
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
+
+module.exports = router;
