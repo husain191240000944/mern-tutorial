15dfe226305def245e99fbd09bdc3f7836c25359
unknown <191240000944@unisnu.ac.id>
2022-07-13 12:55:39 +0700

    buat controllers

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.status(200).json({message: 'Get Goals'})
-})
-
-router.post('/',(req,res)=>{
-    res.status(200).json({message: 'Set Goal'})
-})
-
-router.put('/:id',(req,res)=>{
-    res.status(200).json({message: `Update Goal ${req.params.id}`})
-})
-
-router.delete('/:id',(req,res)=>{
-    res.status(200).json({message: `Delete Goal ${req.params.id}`})
-})
-
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+router.get('/', getGoals)
+router.post('/', setGoal)
+router.put('/:id', updateGoal)
+router.delete('/:id',deleteGoal)
 module.exports = router
