0fce81849055e1039ac7b387f53547813cf53a40
unknown <191240000944@unisnu.ac.id>
2022-07-13 12:38:42 +0700

    buat route file

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,7 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
