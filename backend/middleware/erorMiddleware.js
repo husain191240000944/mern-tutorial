c14537e1e5f8be2fbaf4d74e07791c686df1282e
husain191240000944 <191240000944@unisnu.ac.id>
2022-07-13 13:44:10 +0700

    add goalController

--- backend/middleware/erorMiddleware.js
+++ backend/middleware/erorMiddleware.js
@@ -8,7 +8,6 @@ const errorHandler = (err, req, res, next ) => {
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     })
 }
-
-module.exports = {
+    module.exports = {
     errorHandler
 }
